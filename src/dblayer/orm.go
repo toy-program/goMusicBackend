eff4987b3b23b117b5f291830e70031b919187a1
changhoi <0417kch@naver.com>
2020-06-26 15:59:16 +0900

    model logic

--- src/dblayer/orm.go
+++ src/dblayer/orm.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+// DBORM database orm interface
 type DBORM struct {
 	*gorm.DB
 }
@@ -32,4 +33,34 @@ func (db *DBORM) GetPromos() (products []models.Product, err Error) {
 // GetCustomerByName get customer by name
 func (db *DBORM) GetCustomerByName(firstname, lastname string) (customer models.Customer, err Error) {
 	return customer, db.Where(&models.Customer{FirstName: firstname, LastName: lastname}).Find(&customer).Error
+}
+
+func (db *DBORM) GetCustomerByID(id int) (product models.Product, error) {
+	return product, db.First(&product, id).Error
+}
+
+func (db *DBORM) GetProduct(id int) (product models.Product, error) {
+	return product, db.First(&product, id).Error
+}
+
+func (db *DBORM) AddUser(customer models.Customer) (models.Customer, error) {
+	hashPassword(&customer.Pass)
+	customer.LoggedIn = true
+	return customer, db.Create(&customer).Error
+}
+
+func (db *DBORM) SignInUser (email, pass string) (customer models.Customer, error) {
+	if !checkPassword(pass) {
+		return customer, errors.New("Invalid password")
+	}
+
+	result := db.Table("Customers").Where(&models.Customer{Email: email})
+	err = result.Update("loggedIn", true).Error
+
+
+	if err != nil {
+		return customer, err
+	}
+
+	return customer, result.Find(&customer.Error)
 }
